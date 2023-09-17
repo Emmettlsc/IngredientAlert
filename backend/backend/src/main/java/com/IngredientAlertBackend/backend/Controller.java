5cb90d720acb1d7a379179fd7699d2f9314e0560
Sonny24 <sonnymcmaster@gmail.com>
2023-09-16 21:14:43 -0400

    api thing

--- backend/backend/src/main/java/com/IngredientAlertBackend/backend/Controller.java
+++ backend/backend/src/main/java/com/IngredientAlertBackend/backend/Controller.java
@@ -1,4 +1,8 @@
 package com.IngredientAlertBackend.backend;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +12,23 @@ import org.springframework.web.client.RestTemplate;
 public class Controller {
 
     @GetMapping("/api/code/{param}")
-    public String getCode(@PathVariable String param) {
+    public Ingredient getCode(@PathVariable String param) throws JsonProcessingException {
         String url = "https://world.openfoodfacts.org/api/v2/product/" + param + ".json";
+        //image_url
         RestTemplate temp = new RestTemplate();
-        String result = temp.getForObject(url,String.class);
-        return result;
+        String result = temp.getForObject(url, String.class);
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readTree(result);
+
+        Ingredient newIngredient = new Ingredient();
+        newIngredient.setBrandName(jsonNode.get("id").textValue());
+        //newIngredient.setName(jsonNode.get("product_name").asText());
+        //newIngredient.setIngredients(new String[]{jsonNode.get("ingredients").asText()});
+        newIngredient.setEffect("big ass dick");
+
+        return newIngredient;
     }
+
     @GetMapping("/api/test")
     public String getIngredient() {
         return "hello";
@@ -22,10 +37,23 @@ public class Controller {
 }
 
 class Ingredient {
+    private String brandName;
     private String name;
     private String[] ingredients;
 
     private String effect;
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public String getName() {
         return name;
