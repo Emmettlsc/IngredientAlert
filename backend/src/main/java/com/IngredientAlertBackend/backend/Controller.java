543597ea67d88efac4df022e1839cb2210ddad3a
Sonny24 <sonnymcmaster@gmail.com>
2023-09-17 01:54:09 -0400

    non working post history

--- backend/src/main/java/com/IngredientAlertBackend/backend/Controller.java
+++ backend/src/main/java/com/IngredientAlertBackend/backend/Controller.java
@@ -4,19 +4,22 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.json.*;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 public class Controller {
+
     @Bean
     public Map<String, String> myMap() {
         java.util.Map<String, String> harmfulIngredients = new java.util.HashMap<String, String>();
@@ -32,6 +35,21 @@ public class Controller {
     }
     @GetMapping("/api/code/{param}")
     public JSONArray getCode(@PathVariable String param) throws JsonProcessingException {
+//        String url = "https://world.openfoodfacts.org/api/v2/product/" + param + ".json";
+//        //image_url
+//        RestTemplate temp = new RestTemplate();
+//        JsonNode result = temp.getForObject(url, JsonNode.class);
+//
+//        JsonNode brandName = result.get("product").get("brands");
+//        JsonNode productName = result.get("product").get("product_name");
+//        JsonNode ingredients = result.get("product").get("ingredients_text");
+//        JSONArray arr = new JSONArray();
+//        arr.put(brandName);
+//        arr.put(productName);
+//        arr.put(ingredients);
+//
+//        return arr;
+
         String url = "https://world.openfoodfacts.org/api/v2/product/" + param + ".json";
         //image_url
         RestTemplate temp = new RestTemplate();
@@ -40,12 +58,47 @@ public class Controller {
         JsonNode brandName = result.get("product").get("brands");
         JsonNode productName = result.get("product").get("product_name");
         JsonNode ingredients = result.get("product").get("ingredients_text");
+
+        String[] ingredientsArr = ingredients.textValue().split(", ");
+
+
+        ObjectMapper mapper = new ObjectMapper();
+        List<String> e = new ArrayList<String>();
+        ArrayNode array = mapper.valueToTree(e);
+        ObjectNode ingredientsNode = mapper.valueToTree(ingredients_text);
+        ingredientsNode.putArray("ingredients_text").addAll(array);
+        ingredients = mapper.createObjectNode().set("ingredients_text", ingredientsNode);
+
+        ArrayList<String> harmfulIngredientsList = new ArrayList<String>();
+        ArrayList<String> risksList = new ArrayList<String>();
+
+        for (int i = 0; i < ingredientsArr.length; i++) {
+            if (myMap.containsKey(ingredientsArr[i])) {
+                risksListsList.add(myMap.get(ingredientsArr[i]));
+                // needs list of harmful ingredients
+            }
+        }
+
+        // needs to turn the 2 lists into JsonNode objects
+
         JSONArray arr = new JSONArray();
         arr.put(brandName);
         arr.put(productName);
         arr.put(ingredients);
 
+        JsonNode result = temp.postForObject(url, JSONArray.class);
+
         return arr;
+    }
+
+    @GetMapping("/history")
+    public JSONArray getHistory(@PathVariable String param) throws JsonProcessingException {
+        String url = "https://world.openfoodfacts.org/api/v2/product/" + param + ".json";
+        //image_url
+        RestTemplate temp = new RestTemplate();
+        JSONArray result = temp.getForObject(url, JSONArray.class);
+
+        return result;
     }
 
 }
