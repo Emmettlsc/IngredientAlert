0e63e95ca66e93dd53bf76b471b4f32ce71f00a7
Emmettlsc <emmettlsc@gmail.com>
2023-09-17 04:19:04 -0400

    fixed formatting for barcode scanner

--- frontend/components/BarcodeScanner.tsx
+++ frontend/components/BarcodeScanner.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
+import { Alert } from 'react-native'; // Import at the top of your file
 
 export default function BarcodeScanner() {
     const [hasPermission, setHasPermission] = useState(null);
@@ -17,9 +18,15 @@ export default function BarcodeScanner() {
 
     const handleBarCodeScanned = ({ type, data }) => {
         setScanned(true);
-        alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+        Alert.alert(
+            'Success', // Title (left empty for cleaner appearance)
+            'Navigate to the "Info" tab to view product data.',
+            [
+                { text: 'OK', onPress: () => console.log('OK Pressed') }
+            ],
+            { cancelable: false }
+        );
         const url = 'http://192.168.1.30:8080/api/code/' + data;
-        //Barcode extracted from photo above
         fetch(url)
             .then(response => response.json())
             .then(data => {
@@ -28,7 +35,6 @@ export default function BarcodeScanner() {
             .catch(error => {
 
             });
-
     };
 
     if (hasPermission === null) {
@@ -44,7 +50,11 @@ export default function BarcodeScanner() {
                 onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
                 style={StyleSheet.absoluteFillObject}
             />
-            {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+            {scanned && (
+                <TouchableOpacity style={styles.scanAgainButton} onPress={() => setScanned(false)}>
+                    <Text style={styles.scanAgainText}>Tap to Scan Again</Text>
+                </TouchableOpacity>
+            )}
         </View>
     );
 }
@@ -52,7 +62,19 @@ export default function BarcodeScanner() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center'
     },
+    scanAgainButton: {
+        marginBottom: 50,
+        padding: 15,
+        borderRadius: 8,
+        backgroundColor: '#2196F3',
+        alignItems: 'center'
+    },
+    scanAgainText: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 16
+    }
 });
-
-
